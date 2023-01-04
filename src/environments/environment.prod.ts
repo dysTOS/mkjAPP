d00f4eabd4d6b698232ca62fa63df9415c115666
dysTOS <rolandsams@gmail.com>
2023-01-04 23:07:13 +0100

    update apiUrls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: "https://api.mk-jainzen.at/api/",
+    apiUrl: "https://api-prod-mk-jainzen.217-160-55-172.plesk.page/api/",
     vapidPublicKey:
         "BIsIB28fImumACBAnl09t3HKyJLBJDGKj17Nc7HFKhiRSzFYQdRUBfL0Yfrab8QpEl1ItEajx72I1Wje-yq7j-s",
     prefix: "mkj",
