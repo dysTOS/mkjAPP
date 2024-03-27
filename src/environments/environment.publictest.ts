099c59f2573cd0f340446b89c04ba1ff739c17e4
Sams, Roland <Roland.Sams@united.com>
2024-03-27 11:24:29 +0100

    test

--- src/environments/environment.publictest.ts
+++ src/environments/environment.publictest.ts
@@ -5,7 +5,7 @@ const config = CONFIG_MAP.publictest;
 export const environment = {
   production: config.production,
   apiUrl: config.apiUrl,
-  appTitle: process.env.API_URL,
+  appTitle: window['API_URL'],
   publictest: config.publictest,
   wsHost: config.wsHost,
 };
